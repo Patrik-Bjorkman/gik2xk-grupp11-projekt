d76c5c3d21ea8827ba8e13677132383a0fad6921
sabina-lindell <121024588+sabina-lindell@users.noreply.github.com>
2024-03-15 07:11:34 +0100

    fortsatte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,47 @@
+import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { AppBar, Box, Button, Toolbar, Typography, Container } from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { AppBar, Toolbar, Typography, Button, Box, Grid } from '@mui/material';
 import SaveAsIcon from '@mui/icons-material/SaveAs';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function App() {
-	return (
-		<>
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position='static'>
-					<Toolbar>
-						<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-							<Link to='/'>Webbshop</Link>
-						</Typography>
-						<Button
-							color='secondary'
-							variant='contained'
-							startIcon={<SaveAsIcon />}
-						>
-							<Link to='/products/new'>Skapa Produkt</Link>
-						</Button>
-						<Button
-							sx={{ m: 2 }}
-							color='success'
-							variant='contained'
-							startIcon={<ShoppingCartIcon />}
-						>
-							<Link to='/carts/new'>Kundvagn</Link>
-						</Button>
-					</Toolbar>
-				</AppBar>
-			</Box>
-			<Container component="footer" sx={{ mt: 4 }} maxWidth="xl">
-			<Outlet />
-		</Container>
-		</>
-	);
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              <Link to='/'>Webbshop</Link>
+            </Typography>
+            <Button
+              color='secondary'
+              variant='contained'
+              startIcon={<SaveAsIcon />}
+            >
+              <Link to='/products/new'>Skapa Produkt</Link>
+            </Button>
+            <Button
+              sx={{ m: 2 }}
+              color='success'
+              variant='contained'
+              startIcon={<ShoppingCartIcon />}
+            >
+              <Link to='/carts/new'>Kundvagn</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Outlet />
+      <Grid container component="footer" sx={{ mt: 4 }} justifyContent="center">
+        <Grid item>
+          <Typography variant="body2" color="textSecondary">
+            Kontakta oss
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Typography } from '@mui/material';
+import {Grid, Paper, Typography } from '@mui/material';
 import ProductList from '../components/ProductList';
 
 function Home() {
@@ -7,7 +7,7 @@ function Home() {
 			<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
 				<Grid component='section' item sx={8}>
 					<Paper elevation={8} sx={{ p: 2, mt: 8, borderradius: 5 }}>
-					<Typography variant='h6'>
+					<Typography variant='h5'>
 						 Produkter
 					</Typography>
 				
